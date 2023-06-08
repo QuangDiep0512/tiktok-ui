8dc9bd6eb537b5feec600d3bf00307ad583d9a2f
QuangDiep0512 <diepsang0512@gmail.com>
2023-06-08 23:07:41 +0700

    Cấu hình VSCode với .prettierrc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>HELLO WORLD</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
